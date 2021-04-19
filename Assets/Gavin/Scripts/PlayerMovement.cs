486c4f2818849a5aaa0b2ece585600bc94b82e19
gavin <gavin@DESKTOP-BGG826V>
2021-04-19 09:55:51 -0700

    Very basic player movement. Changed the Horizontal and vertical axis's to make it feel better.

---
+++ Assets/Gavin/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public Vector2 speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed.x, Input.GetAxis("Vertical") * speed.y);
+    }
+}
