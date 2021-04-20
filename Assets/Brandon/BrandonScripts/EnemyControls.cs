714f7d50704906f4860296295215de917da1ad55
Brandon <1707222@mukilteo.wednet.edu>
2021-04-20 09:23:07 -0700

    Added box collider and created enemy stats

---
+++ Assets/Brandon/BrandonScripts/EnemyControls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    private static void EnemyStats()
+    {
+         
+    }
+        public int maxHealth = 50;
+
+    private int _curHealth;
+    public int curHealth;
+      
+
+        
+
+}
+    
+
