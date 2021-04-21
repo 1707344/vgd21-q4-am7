43d7fa1cb727821248000bdb14d9b5acf5e1e1b4
Cadd47 <1707230@mukilteo.wednet.edu>
2021-04-20 18:33:50 -0700

    Enemy gets attacked when pressing "K" near it (check the log). Located on Gavin's scene cuz why not.

---
+++ Assets/Taylor/TaylorScripts/PlayerAttack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private Transform enemy;
+    private float dist;
+    public float howclose;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GameObject.FindWithTag("Enemy").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Vector3.Distance(enemy.position, transform.position);
+
+        if (Input.GetKeyDown("k") && dist <= howclose)
+        {
+            Debug.Log("Player attacked enemy");
+        }
+    }
+}
