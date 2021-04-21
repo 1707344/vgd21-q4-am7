047aa61f64d1aac77ef753fb63d8b2bbc97bf216
gavin <gavin@DESKTOP-BGG826V>
2021-04-21 16:29:33 -0700

    Trying to fix

---
+++ Assets/Gavin/Scripts/EnemyHealth.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float health;
+    public SpriteRenderer sprite;
+
+    public void Start()
+    {
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Hit(2);
+        }
+    }
+
+    public void Hit(float damage)
+    {
+        health -= damage;
+        if(health <= 0)
+        {
+            Die();
+            return;
+        }
+
+
+    }
+
+    public void Flash()
+    {
+        
+    }
+
+    public void Die()
+    {
+
+    }
+}
