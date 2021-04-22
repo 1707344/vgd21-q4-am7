66a6b5e6191c72f970375b0375bf7d5f2d7e2c19
gavin <gavin@DESKTOP-BGG826V>
2021-04-21 19:12:36 -0700

    Got the punching in. Click K to punch.

--- Assets/Gavin/Scripts/PlayerMovement.cs
+++ Assets/Gavin/Scripts/PlayerMovement.cs
@@ -6,15 +6,56 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
     public Vector2 speed;
+
+    public bool isAttacking;
+    bool canAttack = true;
+    GameObject playerHand;
+    SpriteRenderer sprite;
+
     // Start is called before the first frame update
     void Start()
     {
+        sprite = gameObject.GetComponent<SpriteRenderer>();
         rb = gameObject.GetComponent<Rigidbody2D>();
+        playerHand = transform.GetChild(1).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
         rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed.x, Input.GetAxis("Vertical") * speed.y);
+
+        if(rb.velocity.x > 0)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }else if(rb.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * -1, transform.localScale.y, transform.localScale.z);
+        }
+
+        isAttacking = (Input.GetKeyDown(KeyCode.K) && canAttack) || isAttacking;
+
+
+        if (isAttacking)
+        {
+            playerHand.transform.position = Vector2.Lerp(playerHand.transform.position, transform.GetChild(3).position, 0.1f);
+            canAttack = false;
+            Invoke("TurnAttackOff", 0.1f);
+        }
+        else
+        {
+            playerHand.transform.position = Vector2.Lerp(playerHand.transform.position, transform.GetChild(2).position, 0.1f);
+            Invoke("TurnCanAttackOn", 0.15f);
+        }
+    }
+
+    void TurnAttackOff()
+    {
+        isAttacking = false;
+    }
+
+    void TurnCanAttackOn()
+    {
+        canAttack = true;
     }
 }
