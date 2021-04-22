2e6d52d008ecb0a20012e383271d8f20152595de
gavin <gavin@DESKTOP-BGG826V>
2021-04-22 06:16:23 -0700

    Added background and changed colors.

--- Assets/Gavin/Scripts/EnemyHealth.cs
+++ Assets/Gavin/Scripts/EnemyHealth.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     public float health;
-    public SpriteRenderer sprite;
-
+    SpriteRenderer sprite;
     public void Start()
     {
         sprite = gameObject.GetComponent<SpriteRenderer>();
