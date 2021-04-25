583b712ebdc4560df9805542cf0b7593d4e3c8e8
gavin <gavin@DESKTOP-BGG826V>
2021-04-25 07:11:41 -0700

    Created a lighting test scene and made a flicker script for the light.

---
+++ Assets/Gavin/Scripts/LightFlicker.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    Light light;
+    float initLightIntensity;
+    public float maxLightDifference;
+    public float changePerTick;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = gameObject.GetComponent<Light>();
+        initLightIntensity = light.intensity;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        System.Random random = new System.Random();
+        print("Double: " + random.NextDouble());
+        float increment = (float)random.NextDouble() * (changePerTick - (-changePerTick)) + -changePerTick;
+        light.intensity += increment;
+
+        print("Changed by: " + increment);
+
+        if(light.intensity > initLightIntensity + maxLightDifference)
+        {
+            print("Too high");
+            light.intensity = initLightIntensity + maxLightDifference;
+        }else if(light.intensity < -initLightIntensity - maxLightDifference)
+        {
+            print("Too low");
+            light.intensity = -initLightIntensity - maxLightDifference;
+        }
+    }
+}
