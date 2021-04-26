65f8722556af2568867dbd2eff6aefe68336fe28
Brandon <1707222@mukilteo.wednet.edu>
2021-04-26 16:18:55 -0700

    Updated Enemy Follow

--- Assets/Brandon/BrandonScripts/EnemyControls.cs
+++ Assets/Brandon/BrandonScripts/EnemyControls.cs
@@ -5,29 +5,43 @@ using UnityEngine;
 public class EnemyControls : MonoBehaviour
 {
     public Transform player;
-    public float moveSpeed = 5f;
+    public float moveSpeed = 20f;
     private Rigidbody2D rb;
     private Vector2 movement;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb.GetComponent<Rigidbody2D>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     //Update is called once per frame
     private void Update()
     {
         Vector3 direction = player.position - transform.position;
-        float angle = Mathf.Atan(direction.x) * Mathf.Rad2Deg;
-        rb.rotation = angle;
         direction.Normalize();
         movement = direction;
+        moveCharacter(movement);
+        followPlayer();
     }
 
     void moveCharacter(Vector2 direction)
     {
         rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+
+    void followPlayer()
+    {
+        if (transform.position.x < player.position.x)
+        {
+            //Moving to the right
+            transform.localScale = new Vector2(-1, 1);
+        }
+        else
+        {
+            //Moving to the left
+            transform.localScale = new Vector2(1, 1);
+        }
     }
 
 
