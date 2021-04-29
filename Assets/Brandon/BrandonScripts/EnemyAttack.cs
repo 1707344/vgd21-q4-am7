3a3e8e56c426c81cbeb9d08a48b5d20f0a85290f
Brandon <1707222@mukilteo.wednet.edu>
2021-04-28 21:34:31 -0700

    Updated Enemy Attack

---
+++ Assets/Brandon/BrandonScripts/EnemyAttack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField] private float attackDamage = 10f;
+
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerHealth>().UpdateHealth(-attackDamage);
+        }
+    }
+
+}
