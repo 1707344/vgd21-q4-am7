2655c6deb6a927f7581a520bd62c1722b312037e
gavin <gavin@DESKTOP-BGG826V>
2021-05-03 09:59:07 -0700

    Can aim the fire with mouse and click to shoot.

--- Assets/Gavin/Scripts/PlayerMovement.cs
+++ Assets/Gavin/Scripts/PlayerMovement.cs
@@ -24,17 +24,30 @@ public class PlayerMovement : MonoBehaviour
     {
         rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed.x, Input.GetAxis("Vertical") * speed.y);
 
-        if(rb.velocity.x > 0)
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        float angle = Mathf.Atan((mousePosition.y - transform.position.y) / (mousePosition.x - transform.position.x)) * Mathf.Rad2Deg;
+        if(mousePosition.x - transform.position.x < 0)
         {
-            particleSystem.transform.localScale = new Vector3(1, 1, 1);
+            angle += 180;
+        }
+        
+        particleSystem.transform.rotation = Quaternion.Euler(0, 0, angle);
+
+
+        print(angle);
+
+        if (rb.velocity.x > 0)
+        {
+            //particleSystem.transform.localScale = new Vector3(1, 1, 1);
             transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
         }else if(rb.velocity.x < 0)
         {
-            particleSystem.transform.localScale = new Vector3(-1, 1, 1);
+            //particleSystem.transform.localScale = new Vector3(-1, 1, 1);
             transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * -1, transform.localScale.y, transform.localScale.z);
         }
 
-        if (Input.GetKeyDown(KeyCode.K))
+        if (Input.GetKeyDown(KeyCode.K) || Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
             particleSystem.Play();
         }
