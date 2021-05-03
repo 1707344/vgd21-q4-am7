2e4446494952504a914003dd3eb49b8f2aeb2101
Taylor Cadwallader <1707230@mukilteo.wednet.edu>
2021-05-03 10:20:39 -0700

    messing with particles

---
+++ Assets/Taylor/Scripts/NextStage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextStage : MonoBehaviour
+{
+    public GameObject arrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            arrow.SetActive(true);
+            Next();
+        }
+    }
+
+    public void Next()
+    {
+        Destroy(gameObject);
+    }
+}
