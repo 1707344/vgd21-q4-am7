767a376b751c89339ca951ba717ca8a05eec8dde
Brandon <1707222@mukilteo.wednet.edu>
2021-05-16 23:23:37 -0700

    Started making death screen script

---
+++ Assets/Brandon/BrandonScripts/DeathMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathMenu : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
