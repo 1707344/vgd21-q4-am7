cfcc02ceb1c8ad78801f94adbbdcd5066325175c
gavin <gavin@DESKTOP-BGG826V>
2021-05-24 11:14:00 -0700

    Push before merge

--- Assets/Brandon/BrandonScripts/EnemyAttack.cs
+++ Assets/Brandon/BrandonScripts/EnemyAttack.cs
@@ -24,6 +24,7 @@ public class EnemyAttack : MonoBehaviour
             {
                 other.gameObject.GetComponent<PlayerHealth>().UpdateHealth(-attackDamage);
                 canAttack = 0f;
+                
             }
             else
             {
